97588a3996b249d1a394b53ed88e4cb76d3032b8
tjfgk97 <tjfgk97@gmail.com>
2024-12-02 21:01:33 +0900

    예외처리(IF문, try catch문)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 const { MongoClient } = require('mongodb');
@@ -30,23 +30,34 @@ app.get('/news', (요청, 응답) => {
     응답.send('오늘 비옴')
 })
 
-app.get('/list', async (요청, 응답)=>{
+app.get('/list', async (요청, 응답) => {
     let result = await db.collection('post').find().toArray()
     console.log(result[0].title);
-    응답.render('list.ejs', { posts : result })
+    응답.render('list.ejs', { posts: result })
 })
 
 app.get('/time', (요청, 응답) => {
-    응답.render('time.ejs', { time : new Date()})
+    응답.render('time.ejs', { time: new Date() })
 })
 
 app.get('/post', (요청, 응답) => {
     응답.render('post.ejs')
 })
 
-app.post('/add', (요청, 응답) => {
-    응답.send('전송완료')
+app.post('/add', async (요청, 응답) => {
     let title = 요청.body.title;
     let content = 요청.body.content;
-    db.collection('post').insertOne({title: title, content: content});
+
+    try {
+        if (요청.body.title == '') {
+            응답.send('제목 입력하세요.')
+        } else {
+            await db.collection('post').insertOne({ title: title, content: content });
+            응답.redirect('/list')
+        }
+    } catch (e) {
+        console.log(e);
+        응답.status(500).send('ERROR')
+    }
+
 })
